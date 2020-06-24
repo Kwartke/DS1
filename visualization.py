1bd040ba49fc0aa9c3f7ca1a24036b7fcde48d1d
Hanna <mrtnshanna@gmail.com>
2020-06-24 09:55:59 +0200

    fix plotting

--- visualization.py
+++ visualization.py
@@ -7,6 +7,8 @@ df: pd.DataFrame = pd.read_csv('output/merged.csv', engine='python')
 
 df_merged = df[~np.isinf(df)].copy()
 df_merged.dropna(inplace=True)
+df_merged = df_merged[(df_merged > 0).all(1)].copy()
+
 
 pgcodes = pgc.Nominatim('DE')
 df_plz = df_merged["plz"].to_numpy()
@@ -14,55 +16,31 @@ plz_strings = ["%.i" % number for number in df_plz]
 long_lat = pgcodes.query_postal_code(plz_strings)
 long_lat = long_lat[['longitude', 'latitude']]
 
-frames = [df_merged, long_lat]
-result = pd.concat(frames, axis=1)
-
-print(result['Zimmer'])
+#frames = [df_merged, long_lat]
+#result = pd.concat(frames, axis=1)
 
 plt.figure()
-plt.plot(result[['latitude', 'longitude']])
-plt.ylabel('Longitude', fontsize=12)
-plt.xlabel('Latitude', fontsize=12)
-#plt.draw()
-
-plt.figure()
-plt.scatter(x=result['avg_Mietpreis_pro_m2'], y=result['Wohnflaeche__m�_'])
 ax = plt.gca()
 ax.get_yaxis().get_major_formatter().set_scientific(False)
-plt.scatter(result['avg_Mietpreis_pro_m2'], result['longitude'])
-plt.title("Mietpreis_pro_m2 vs Ort")
-#plt.draw()
-
-plt.figure()
-plt.scatter(result['avg_Mietpreis_pro_m2'], result['latitude'])
-plt.xlabel("Price")
-plt.ylabel('Latitude')
-plt.title("Latitude vs Mietpreis")
+ax.set(xlim=(0, 30), ylim=(0, 300))
+plt.scatter(df_merged['avg_Mietpreis_pro_m2'], df_merged['Wohnflaeche__m�_'])
+plt.xlabel("Durchschnittlicher Mietpreis pro qm")
+plt.ylabel("Wohnfläche")
 #plt.show()
 
 plt.figure()
-plt.scatter(result['Zimmer'], result['avg_Mietpreis_pro_m2'])
-plt.title("Mietpreis_pro_m2 und Anzahl der Zimmer")
-plt.xlabel("Zimmer")
-plt.ylabel("Mietpreis")
-#plt.draw()
-
-plt.scatter(x=result['avg_Mietpreis_pro_m2'],y=result['Wohnflaeche__m�_'])
 ax = plt.gca()
+ax.set(xlim=(0, 5000), ylim=(0, 1000))
 ax.get_yaxis().get_major_formatter().set_scientific(False)
-#plt.draw()
+plt.scatter(df_merged['avg_Kaufpreis_pro_m2'],df_merged['Wohnflaeche__m�_'])
+plt.xlabel("Durchschnittlicher Kaufpreis pro qm")
+plt.ylabel("Wohnfläche")
+#plt.show()
 
-plt.scatter(result['avg_Mietpreis_pro_m2'],result['longitude'])
-plt.title("Mietpreis_pro_m2 vs Ort")
-
-
-plt.scatter(result['avg_Mietpreis_pro_m2'],result['latitude'])
-plt.xlabel("Price")
-plt.ylabel('Latitude')
-plt.title("Latitude vs Mietpreis")
-
-plt.scatter(result['Zimmer'],result['avg_Mietpreis_pro_m2'])
-plt.title("Mietpreis_pro_m2 und Anzahl der Zimmer")
-plt.xlabel("Zimmer")
-plt.ylabel("Mietpreis")
+plt.figure()
+ax = plt.gca()
+ax.set(xlim=(0, 10), ylim=(0, 40))
+plt.scatter(df_merged['Zimmer'],df_merged['avg_Mietpreis_pro_m2'])
+plt.xlabel("Anzahl Zimmer")
+plt.ylabel("Durchschnittlicher Kaufpreis pro qm")
 #plt.show()
